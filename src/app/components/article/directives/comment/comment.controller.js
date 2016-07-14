8851e8ec0a9beba17cd0e0326fa1fb5bb65893f6
Drm1804 <drm-a@yandex.ru>
2016-07-14 15:56:08 +0500

    Сделал скрытие формы добавление комментария, пользователь нажал ответить в другом метесте

--- src/app/components/article/directives/comment/comment.controller.js
+++ src/app/components/article/directives/comment/comment.controller.js
@@ -4,18 +4,39 @@
   angular.module('btest')
     .controller('CommentController', CommentController);
 
-
-  function CommentController() {
+  CommentController.$inject = ['$scope', '$rootScope'];
+  function CommentController($scope, $rootScope) {
     var vm = this;
     vm.showForm = false;
 
+    // Поскольку контроллер дочерний, можно взять данные из родительского scope
+    vm.commentData = $scope.$parent.item;
+
+    console.log(vm.commentData);
+
     vm.toggleForm = toggleForm;
+
+
+    // Получаем имен, в нем смотрим id родительского комментария
+    // если он совпадает с текущим, то ничешего не делаем
+    $rootScope.$on('comment:closeForm', function(event, data){
+      console.log( data.id )
+      if(data.id !== vm.commentData.id){
+        vm.showForm = false;
+      }
+
+    });
+
 
     function toggleForm(){
       if(vm.showForm){
         vm.showForm = false;
       } else {
         vm.showForm = true;
+
+        // Создаем ивент сворачивания всех форм
+        // и передаем в него id комментария, к которому будет добавлен дочерний коммент
+        $rootScope.$emit('comment:closeForm', { id: vm.commentData.id });
       }
     }
   }
