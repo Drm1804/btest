73fe19f4902a30eb3c1664b29ea7a1577ea364a1
Устинов Алексей Геннадьевич <ustinov@nports.ru>
2016-07-18 08:56:59 +0500

    Добавил регулярок, и протестировал имеющиеся

--- src/app/components/parser/parser.service.js
+++ src/app/components/parser/parser.service.js
@@ -1,84 +1,88 @@
 (function () {
-    'use strict';
+  'use strict';
 
 
-    angular.module('btest')
-        .service('parserService', parserService);
+  angular.module('btest')
+    .service('parserService', parserService);
 
-    parserService.$inject = [];
-    function parserService(){
-        var _this = this;
-        _this.data = '';
-        _this.masks = {
-            newline: /\n+/g,
-            h1: /^#[а-яА-ЯёЁ0-1\w]+$/g,
-            h2: /^##[а-яА-ЯёЁ0-1\w]+$/g,
-            h3: /^###[а-яА-ЯёЁ0-1\w]+$/g,
-            h4: /^####[а-яА-ЯёЁ0-1\w]+$/g,
-            h5: /^#####[а-яА-ЯёЁ0-1\w]+$/g,
-            h6: /^######[а-яА-ЯёЁ0-1\w]+$/g
-        };
+  parserService.$inject = [];
+  function parserService() {
+    var _this = this;
+    _this.data = '';
+    _this.masks = {
+      newline: /\n+/g,
+      h1: /^#[а-яА-ЯёЁ0-1\s\w]+$/gm,
+      h2: /^#{2}[а-яА-ЯёЁ0-1\s\w]+$/gm,
+      h3: /^#{3}[а-яА-ЯёЁ0-1\s\w]+$/gm,
+      h4: /^#{4}[а-яА-ЯёЁ0-1\s\w]+$/gm,
+      h5: /^#{5}[а-яА-ЯёЁ0-1\s\w]+$/gm,
+      h6: /^#{6}[а-яА-ЯёЁ0-1\s\w]+$/gm,
+      code: /`([^`]+)`/gm,
+      italic: /\s\*([^\*]+)\*\s/gm,
+      bold: /\s\*{2}([^\*]+)\*{2}\s/gm
 
-        _this.parse = parse;
+    };
 
-
-        /*
-         * Приватный метод separateOneLineText
-         * Метод проверяет простой текст на наличие в нем md-элементов
-         *
-         * Аргументы:
-         *   data - массив строк
-         *
-         *
-         *
-         * Возвращает объект c 2-мя свойтвами:
-         *  simple - массив однострочных элементов
-         *  complex - массив массивов многострочных элементов
-         * */
-
-        function separateOneLineText(data){
-
-            var simpleArr = [];
-            var complexArr = [];
-
-            for(var item in data){
-
-            }
-        }
+    _this.parse = parse;
 
 
-        /*
-         * Приватный метод partitionLine
-         *
-         * Аргументы:
-         *   data - строка с markdown текстом
-         *
-         *
-         * Возвращает массив с линиями
-         * */
+    /*
+     * Приватный метод separateOneLineText
+     * Метод проверяет простой текст на наличие в нем md-элементов
+     *
+     * Аргументы:
+     *   data - массив строк
+     *
+     *
+     *
+     * Возвращает объект c 2-мя свойтвами:
+     *  simple - массив однострочных элементов
+     *  complex - массив массивов многострочных элементов
+     * */
 
-        function partitionLine(data){
-            return data.split( _this.masks.newline);
-        }
+    function separateOneLineText(data) {
 
-        /*
-        * Открытый метод parse
-        *
-        * Аргументы:
-        *   data - строка с markdown текстом
-        *
-        *
-        * Возвращает строку с html - тестом
-        * */
+      var simpleArr = [];
+      var complexArr = [];
 
-        function parse(data){
-            var arrayData = partitionLine(data);
+      for (var item in data) {
 
-            var complexArr = separateOneLineText(arrayData);
-            console.log(complexArr)
-
-
-        }
+      }
     }
+
+
+    /*
+     * Приватный метод partitionLine
+     *
+     * Аргументы:
+     *   data - строка с markdown текстом
+     *
+     *
+     * Возвращает массив с линиями
+     * */
+
+    function partitionLine(data) {
+      return data.split(_this.masks.newline);
+    }
+
+    /*
+     * Открытый метод parse
+     *
+     * Аргументы:
+     *   data - строка с markdown текстом
+     *
+     *
+     * Возвращает строку с html - тестом
+     * */
+
+    function parse(data) {
+      var arrayData = partitionLine(data);
+
+      var complexArr = separateOneLineText(arrayData);
+      console.log(complexArr)
+
+
+    }
+  }
 
 })();
