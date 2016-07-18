3a93a79b456d9bcf33c3ff0d79882d0e18a6dcc3
Устинов Алексей Геннадьевич <ustinov@nports.ru>
2016-07-18 10:53:46 +0500

    Доделал регулярки

--- src/app/components/parser/parser.service.js
+++ src/app/components/parser/parser.service.js
@@ -11,12 +11,13 @@
     _this.data = '';
     _this.masks = {
       newline: /\n+/g,
-      h1: /^#[а-яА-ЯёЁ0-1\s\w]+$/gm,
-      h2: /^#{2}[а-яА-ЯёЁ0-1\s\w]+$/gm,
-      h3: /^#{3}[а-яА-ЯёЁ0-1\s\w]+$/gm,
-      h4: /^#{4}[а-яА-ЯёЁ0-1\s\w]+$/gm,
-      h5: /^#{5}[а-яА-ЯёЁ0-1\s\w]+$/gm,
-      h6: /^#{6}[а-яА-ЯёЁ0-1\s\w]+$/gm,
+      h1: /^#[а-яА-ЯёЁ0-1\s\w][^\n]+$/gm,
+      h2: /^#{2}[а-яА-ЯёЁ0-1\s\w][^\n]+$/gm,
+      h3: /^#{3}[а-яА-ЯёЁ0-1\s\w][^\n]+$/gm,
+      h4: /^#{4}[а-яА-ЯёЁ0-1\s\w][^\n]+$/gm,
+      h5: /^#{5}[а-яА-ЯёЁ0-1\s\w][^\n]+$/gm,
+      h6: /^#{6}[а-яА-ЯёЁ0-1\s\w][^\n]+$/gm,
+      text: /^[^<][а-яА-ЯёЁ0-1\s\w][^\n]+$/gm,
       code: /`([^`]+)`/gm,
       italic: /\s\*([^\*]+)\*\s/gm,
       bold: /\s\*{2}([^\*]+)\*{2}\s/gm
@@ -39,15 +40,43 @@
     function parseMD(data) {
 
       data = data.replace( _this.masks.h1, function(result){
-
         result = result.replace('\#', '');
         return '<h1>'+ result+ '</h1>'
       });
-      
+
+      data = data.replace( _this.masks.h2, function(result){
+        result = result.replace('\##', '');
+        return '<h2>'+ result+ '</h2>'
+      });
+
+      data = data.replace( _this.masks.h3, function(result){
+        result = result.replace('\###', '');
+        return '<h3>'+ result+ '</h3>'
+      });
+
+      data = data.replace( _this.masks.h4, function(result){
+
+        result = result.replace('\####', '');
+        return '<h4>'+ result+ '</h4>'
+      });
+
+      data = data.replace( _this.masks.h5, function(result){
+        result = result.replace('\#####', '');
+        return '<h5>'+ result+ '</h5>'
+      });
+
+      data = data.replace( _this.masks.h6, function(result){
+        result = result.replace('\######', '');
+        return '<h6>'+ result+ '</h6>'
+      });
+
+      data = data.replace( _this.masks.text, '<p>$&</p>');
+
       data = data.replace( _this.masks.italic, '<code>$1</code>');
 
+      data = data.replace( _this.masks.bold, '<code>$1</code>');
 
-      console.log(data)
+      data = data.replace( _this.masks.code, '<code>$1</code>');
 
       return data
 
